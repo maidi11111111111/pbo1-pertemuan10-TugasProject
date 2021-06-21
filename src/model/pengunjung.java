f000f25306412dc00ac80063448e7a60ea8f0ca7
maidi11111111111 <maidi11.mm@gmail.com>
2021-06-21 18:30:38 +0800

    Initial commit

---
+++ src/model/pengunjung.java
@@ -0,0 +1,20 @@
+import java.time.*;
+package model;
+
+public class pengunjung {
+    // atribut
+    String idPengujung;
+    String nama;
+    String alamat;
+    LocalDate tglLahir;
+    int denda;
+    String tglPeminjaman;
+    String tglPengembalian;
+    String namaBuku;
+
+//const
+}
+    // method menghitung lama peminjaman
+   long hitungdenda(){
+
+   }
